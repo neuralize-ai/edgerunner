c0a919dca0e5d26540ffbf1407463fd2ede85b91
Ciaran O' Rourke <ciaranborourke-dev@proton.me>
2024-06-21 04:30:54 +0100

    Add tflite as a dependency

--- conanfile.py
+++ conanfile.py
@@ -11,6 +11,7 @@ class Recipe(ConanFile):
     def requirements(self):
         self.requires("fmt/10.2.1")
         self.requires("span-lite/0.11.0", transitive_headers=True)
+        self.requires("tensorflow-lite/2.12.0")
 
     def build_requirements(self):
         self.test_requires("catch2/3.6.0")
