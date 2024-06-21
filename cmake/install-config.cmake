3aead1c5938a0528c1e35208d9f499c4cb947d41
Ciaran O' Rourke <ciaranborourke-dev@proton.me>
2024-06-21 04:30:31 +0100

    Add span-lite as a dependency

--- cmake/install-config.cmake
+++ cmake/install-config.cmake
@@ -1,4 +1,5 @@
 include(CMakeFindDependencyMacro)
 find_dependency(fmt)
+find_dependency(span-lite)
 
 include("${CMAKE_CURRENT_LIST_DIR}/edgerunnerTargets.cmake")
--- conanfile.py
+++ conanfile.py
@@ -10,6 +10,7 @@ class Recipe(ConanFile):
 
     def requirements(self):
         self.requires("fmt/10.2.1")
+        self.requires("span-lite/0.11.0", transitive_headers=True)
 
     def build_requirements(self):
         self.test_requires("catch2/3.6.0")
