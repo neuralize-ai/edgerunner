b1f96586fdf7a79d176a8bfd4cac49736fabf006
Ciaran O' Rourke <ciaranborourke-dev@proton.me>
2024-06-23 15:32:59 -0700

    Add OpenCV as a dependency

--- conanfile.py
+++ conanfile.py
@@ -12,6 +12,17 @@ class Recipe(ConanFile):
         self.requires("fmt/10.2.1")
         self.requires("span-lite/0.11.0", transitive_headers=True)
         self.requires("tensorflow-lite/2.12.0")
+        self.requires("opencv/4.9.0")
 
     def build_requirements(self):
         self.test_requires("catch2/3.6.0")
+
+    def configure(self):
+        self.options["opencv"].with_quirc = False
+        self.options["opencv"].with_ffmpeg = False
+        self.options["opencv"].with_tesseract = False
+        self.options["opencv"].with_openexr = False
+        self.options["opencv"].with_tiff = False
+        self.options["opencv"].with_webp = False
+        self.options["opencv"].with_msmf = False
+        self.options["opencv"].with_msmf_dxva = False
