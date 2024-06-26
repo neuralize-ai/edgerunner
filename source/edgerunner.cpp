ee97c1daac668066692709a620abd695c00842f7
Ciaran O' Rourke <ciaranborourke-dev@proton.me>
2024-06-25 18:42:57 -0700

    Fix model filename extension query

--- source/edgerunner.cpp
+++ source/edgerunner.cpp
@@ -1,12 +1,13 @@
 #include "edgerunner/edgerunner.hpp"
 
+#include "edgerunner/model.hpp"
 #include "edgerunner/tflite/model.hpp"
 
 namespace edge {
 
 auto createModel(const std::filesystem::path& modelPath)
     -> std::unique_ptr<Model> {
-    const auto modelExtension = modelPath.extension().string();
+    const auto modelExtension = modelPath.extension().string().substr(1);
 
     if (modelExtension == "tflite") {
         return std::make_unique<tflite::ModelImpl>(modelPath);
