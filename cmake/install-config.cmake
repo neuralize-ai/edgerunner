cf5d6abe603af2a2d0548facadc8c40b3a09ff2c
Ciaran O' Rourke <ciaranborourke-dev@proton.me>
2024-06-27 00:15:09 -0700

    Remove fmt and opencv from install-config

--- cmake/install-config.cmake
+++ cmake/install-config.cmake
@@ -1,7 +1,5 @@
 include(CMakeFindDependencyMacro)
-find_dependency(fmt)
 find_dependency(span-lite)
 find_dependency(tensorflowlite)
-find_dependency(opencv)
 
 include("${CMAKE_CURRENT_LIST_DIR}/edgerunnerTargets.cmake")
