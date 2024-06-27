c9c99397d033b958c51d822ead8190739a506f66
Ciaran O' Rourke <ciaranborourke-dev@proton.me>
2024-06-27 00:17:33 -0700

    Add package_info

--- conanfile.py
+++ conanfile.py
@@ -93,3 +93,18 @@ class EdgerunnerRecipe(ConanFile):
     def package(self):
         cmake = CMake(self)
         cmake.install()
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "edgerunner")
+        self.cpp_info.set_property("cmake_target_name", "edgerunner::edgerunner")
+
+        self.cpp_info.names["cmake_find_package"] = "edgerunner"
+        self.cpp_info.names["cmake_find_package_multi"] = "edgerunner"
+
+        defines = []
+
+        if self.options.gpu:
+            defines.append("edgerunner_ENABLE_GPU")
+
+        self.cpp_info.defines = defines
+        self.cpp_info.libs = ["edgerunner"]
