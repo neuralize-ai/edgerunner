aed7ec1ea8dc23175e15018c93ff8b1784a9cad9
Ciaran O' Rourke <ciaranborourke-dev@proton.me>
2024-07-01 13:05:14 -0700

    Test that getInputs and getOutputs provide access to the same underlying buffers getInput and getOutput

--- test/source/tflite_test.cpp
+++ test/source/tflite_test.cpp
@@ -15,47 +15,39 @@
 TEST_CASE("Tflite default runtime (CPU)", "[tflite][cpu]") {
     const std::string modelPath = "models/tflite/mobilenet_v3_small.tflite";
     auto model = edge::createModel(modelPath);
-
     REQUIRE(model != nullptr);
-
     REQUIRE(std::string {"mobilenet_v3_small"} == model->name());
-
     REQUIRE(model->getDelegate() == edge::DELEGATE::CPU);
 
+    const auto inputs = model->getInputs();
     const auto numInputs = model->getNumInputs();
-
     REQUIRE(numInputs == 1);
+    REQUIRE(numInputs == inputs.size());
 
+    const auto outputs = model->getOutputs();
     const auto numOutputs = model->getNumOutputs();
-
     REQUIRE(numOutputs == 1);
+    REQUIRE(numOutputs == outputs.size());
 
     auto input = model->getInput(0);
-
     REQUIRE(input->getName() == "image_tensor");
-
     REQUIRE(input->getDimensions() == std::vector<size_t> {1, 224, 224, 3});
-
     REQUIRE(input->getType() == edge::TensorType::FLOAT32);
+    REQUIRE(input.get() == inputs[0].get());
 
     auto inputData = input->getTensorAs<float>();
-
     REQUIRE(inputData.size() == input->getSize());
 
     auto output = model->getOutput(0);
-
     REQUIRE(output->getName() == "output_0");
-
     REQUIRE(output->getDimensions() == std::vector<size_t> {1, 1000});
-
     REQUIRE(output->getType() == edge::TensorType::FLOAT32);
+    REQUIRE(output.get() == outputs[0].get());
 
     auto outputData = output->getTensorAs<float>();
-
     REQUIRE(outputData.size() == output->getSize());
 
     const auto executionStatus = model->execute();
-
     REQUIRE(executionStatus == edge::STATUS::SUCCESS);
 
     BENCHMARK("execution") {
@@ -64,9 +56,7 @@ TEST_CASE("Tflite default runtime (CPU)", "[tflite][cpu]") {
 
     /* verify output buffer is persistent across execution */
     const auto outputDataAfter = model->getOutput(0)->getTensorAs<float>();
-
     const auto outputMse = meanSquaredError(outputData, outputDataAfter);
-
     CAPTURE(outputMse);
     REQUIRE(outputMse < std::numeric_limits<float>::epsilon());
 }
