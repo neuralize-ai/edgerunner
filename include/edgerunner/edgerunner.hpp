b82d6b8c463661c5906e62fa086072aa91cff744
Ciaran O' Rourke <ciaranborourke-dev@proton.me>
2024-07-01 12:58:07 -0700

    Add createModel overload for model buffer instead of model file path

--- include/edgerunner/edgerunner.hpp
+++ include/edgerunner/edgerunner.hpp
@@ -19,12 +19,29 @@ namespace edge {
  * This function takes a file path to a model and creates a new Model object
  * from it.
  *
- * This is the intended way to instantiate a Model using the edgerunner library
+ * createModel() is the intended way to instantiate a Model using the edgerunner
+ * library
  *
  * @param modelPath The file path to the model file
  * @return A unique pointer to the created Model object
  */
 auto EDGERUNNER_EXPORT createModel(const std::filesystem::path& modelPath)
+    -> std::unique_ptr<Model>;
+
+/**
+ * @brief Function to create a model from a given buffer
+ *
+ * This function takes a buffer of a model and creates a new Model object
+ * from it.
+ *
+ * createModel() is the intended way to instantiate a Model using the edgerunner
+ * library
+ *
+ * @param modelBuffer The buffer of the model file
+ * @return A unique pointer to the created Model object
+ */
+auto EDGERUNNER_EXPORT createModel(const nonstd::span<uint8_t>& modelBuffer,
+                                   const std::string& modelExtension = "tflite")
     -> std::unique_ptr<Model>;
 
 }  // namespace edge
