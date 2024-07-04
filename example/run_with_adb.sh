3bbfdff9c41ac36a0d8bd7d3cebb1e2ceacd4a03
Ciaran O' Rourke <ciaranborourke-dev@proton.me>
2024-07-03 23:16:52 -0700

    Modify run_with_adb.sh to find QNN shared libraries at runtime

--- example/run_with_adb.sh
+++ example/run_with_adb.sh
@@ -9,48 +9,48 @@ EXAMPLE=""
 BUILD_DIR=""
 
 while [[ $# -gt 0 ]]; do
-	case $1 in
-	-h | --help)
-		echo "$usage"
-		exit
-		;;
-	-e | --example)
-		shift
-		if [[ -z "$1" || "$1" == --* ]]; then
-			echo "Error: -e,--example requires an argument."
-			echo "$usage"
-			exit 1
-		fi
-		EXAMPLE="$1"
-		shift
-		;;
-	-b | --build-dir)
-		shift
-		if [[ -z "$1" || "$1" == --* ]]; then
-			echo "Error: -b,--build-dir requires an argument."
-			echo "$usage"
-			exit 1
-		fi
-		BUILD_DIR="$1"
-		shift
-		;;
-	*)
-		echo "Unsupported option: $1"
-		exit 1
-		;;
-	esac
+    case $1 in
+    -h | --help)
+        echo "$usage"
+        exit
+        ;;
+    -e | --example)
+        shift
+        if [[ -z "$1" || "$1" == --* ]]; then
+            echo "Error: -e,--example requires an argument."
+            echo "$usage"
+            exit 1
+        fi
+        EXAMPLE="$1"
+        shift
+        ;;
+    -b | --build-dir)
+        shift
+        if [[ -z "$1" || "$1" == --* ]]; then
+            echo "Error: -b,--build-dir requires an argument."
+            echo "$usage"
+            exit 1
+        fi
+        BUILD_DIR="$1"
+        shift
+        ;;
+    *)
+        echo "Unsupported option: $1"
+        exit 1
+        ;;
+    esac
 done
 
 if [[ -z "$EXAMPLE" ]]; then
-	echo "Error: -e,--example is a mandatory argument."
-	echo "$usage"
-	exit 1
+    echo "Error: -e,--example is a mandatory argument."
+    echo "$usage"
+    exit 1
 fi
 
 if [[ -z "$BUILD_DIR" ]]; then
-	echo "Error: -b,--build-dir is a mandatory argument."
-	echo "$usage"
-	exit 1
+    echo "Error: -b,--build-dir is a mandatory argument."
+    echo "$usage"
+    exit 1
 fi
 
 MODELS_DIR="${ROOT_DIR}/models"
@@ -66,6 +66,6 @@ adb push --sync "${BUILD_DIR}" "${APP_BUILD_DIR}"
 adb push --sync "${MODELS_DIR}" "${APP_EXAMPLE_DIR}"
 adb push --sync "${IMAGES_DIR}" "${APP_EXAMPLE_DIR}"
 
-adb shell "cd ${APP_EXAMPLE_DIR} && ./${EXAMPLE}"
+adb shell "cd ${APP_EXAMPLE_DIR} && LD_LIBRARY_PATH=.. ADSP_LIBRARY_PATH=.. ./${EXAMPLE}"
 
-adb shell "rm -rf ${APP_ROOT_DIR}"
+# adb shell "rm -rf ${APP_ROOT_DIR}"
