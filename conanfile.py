341531ebd2ddb8a4a7b9c44f428ea1848ffa5103
Ciaran O' Rourke <ciaranborourke-dev@proton.me>
2024-07-05 23:13:10 -0700

    Disable with_wayland opencv option

--- conanfile.py
+++ conanfile.py
@@ -77,6 +77,7 @@ class EdgerunnerRecipe(ConanFile):
             self.options["opencv"].with_webp = False
             self.options["opencv"].with_msmf = False
             self.options["opencv"].with_msmf_dxva = False
+            self.options["opencv"].with_wayland = False
             self.options["opencv"].with_opencl = True
 
     def generate(self):
