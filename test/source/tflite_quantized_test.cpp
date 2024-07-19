c72069beb9fdabc064ce3d195000fdc4f6c2b18c
Ciaran O' Rourke <ciaranborourke-dev@proton.me>
2024-07-19 12:44:55 -0700

    Add test for quantized model

--- test/source/tflite_quantized_test.cpp
+++ test/source/tflite_quantized_test.cpp
@@ -1,8 +1,8 @@
 #include <cstddef>
+#include <cstdint>
 #include <string>
 #include <vector>
 
-#include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 
 #include "edgerunner/edgerunner.hpp"
@@ -10,17 +10,19 @@
 #include "edgerunner/tensor.hpp"
 #include "utils.hpp"
 
-TEST_CASE("Tflite default runtime (CPU)", "[tflite][cpu]") {
-    const std::string modelPath = "models/tflite/mobilenet_v3_small.tflite";
+TEST_CASE("Tflite default runtime quantized (CPU)",
+          "[tflite][cpu][quantized]") {
+    const std::string modelPath =
+        "models/tflite/mobilenet_v3_large_quantized.tflite";
 
     auto model = edge::createModel(modelPath);
     REQUIRE(model != nullptr);
-    REQUIRE(std::string {"mobilenet_v3_small"} == model->name());
+    REQUIRE(std::string {"mobilenet_v3_large_quantized"} == model->name());
 
     model->applyDelegate(edge::DELEGATE::CPU);
     REQUIRE(model->getDelegate() == edge::DELEGATE::CPU);
 
-    REQUIRE(model->getPrecision() == edge::TensorType::FLOAT16);
+    REQUIRE(model->getPrecision() == edge::TensorType::UINT8);
 
     const auto inputs = model->getInputs();
     const auto numInputs = model->getNumInputs();
@@ -35,22 +37,22 @@ TEST_CASE("Tflite default runtime (CPU)", "[tflite][cpu]") {
     auto input = model->getInput(0);
     REQUIRE(input->getName() == "image_tensor");
     REQUIRE(input->getDimensions() == std::vector<size_t> {1, 224, 224, 3});
-    REQUIRE(input->getType() == edge::TensorType::FLOAT32);
+    REQUIRE(input->getType() == edge::TensorType::UINT8);
     REQUIRE(input.get() == inputs[0].get());
 
-    auto inputData = input->getTensorAs<float>();
+    auto inputData = input->getTensorAs<uint8_t>();
     REQUIRE(inputData.size() == input->getSize());
 
     auto badInput = model->getInput(1);
     REQUIRE(badInput == nullptr);
 
     auto output = model->getOutput(0);
-    REQUIRE(output->getName() == "output_0");
+    REQUIRE(output->getName() == "class_logits");
     REQUIRE(output->getDimensions() == std::vector<size_t> {1, 1000});
-    REQUIRE(output->getType() == edge::TensorType::FLOAT32);
+    REQUIRE(output->getType() == edge::TensorType::UINT8);
     REQUIRE(output.get() == outputs[0].get());
 
-    auto outputBuffer = output->getTensorAs<float>();
+    auto outputBuffer = output->getTensorAs<uint8_t>();
     REQUIRE(outputBuffer.size() == output->getSize());
 
     auto badOutput = model->getOutput(1);
@@ -59,12 +61,8 @@ TEST_CASE("Tflite default runtime (CPU)", "[tflite][cpu]") {
     const auto executionStatus = model->execute();
     REQUIRE(executionStatus == edge::STATUS::SUCCESS);
 
-    BENCHMARK("execution") {
-        return model->execute();
-    };
-
     /* verify output buffer is persistent across execution */
-    const auto newOutputBuffer = model->getOutput(0)->getTensorAs<float>();
+    const auto newOutputBuffer = model->getOutput(0)->getTensorAs<uint8_t>();
     REQUIRE(outputBuffer.data() == newOutputBuffer.data());
     REQUIRE(outputBuffer.size() == newOutputBuffer.size());
 }
